d382e407e969d4ae71f3da59a61b079a74c1dcbc
feixy <kross.fisher@gmail.com>
2023-06-24 14:22:40 +0000

    Add clone for kross-fisher web repo

--- tools/01-basic-setup.sh
+++ tools/01-basic-setup.sh
@@ -3,11 +3,14 @@ if [ ! -d ~/fishes ]; then
     cd && git clone git@github.com:kross-fisher/fishes.git
 fi
 
+if [ ! -d ~/kross-fisher ]; then
+    cd && git clone git@github.com:kross-fisher/kross-fisher.github.io.git kross-fisher
+fi
+
 sudo apt update
-sudo apt install vim git nginx -y
+sudo apt install vim git gcc g++ nginx -y
 
 cp /usr/share/vim/vim*/vimrc_example.vim ~/.vimrc
-#wget --no-check-certificate https://www.melabear.cn/download/vimrc.txt -q -O - | tail -n22 >> ~/.vimrc
 cat ~/fishes/vimrc.txt | tail -n22 >> ~/.vimrc
 
 git config --global core.editor vim
