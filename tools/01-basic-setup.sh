1fb37c5e2809b4bd2e3081336bcc2993a8ec1485
feixy <kross.fisher@gmail.com>
2023-06-24 14:00:00 +0000

    Add git config settings & domain name checking

--- tools/01-basic-setup.sh
+++ tools/01-basic-setup.sh
@@ -9,3 +9,7 @@ sudo apt install vim git nginx -y
 cp /usr/share/vim/vim*/vimrc_example.vim ~/.vimrc
 #wget --no-check-certificate https://www.melabear.cn/download/vimrc.txt -q -O - | tail -n22 >> ~/.vimrc
 cat ~/fishes/vimrc.txt | tail -n22 >> ~/.vimrc
+
+git config --global core.editor vim
+git config --global user.name feixy
+git config --global user.email kross.fisher@gmail.com
--- tools/02-certbot-mkcert.sh
+++ tools/02-certbot-mkcert.sh
@@ -1,14 +1,29 @@
 
-sudo ufw allow 22
-sudo ufw allow 80
-sudo ufw allow 443
-sudo ufw status
+my_domain=''
+read -p 'Input your domain name: ' my_domain
 
-sudo apt install -y nginx snapd
-sudo snap install core
-sudo snap refresh core
-sudo snap install certbot --classic
+function do_setup() {
+    if [ -z "$my_domain" ]; then
+        echo "No domain name provided, exit ..."
+        return
+    fi
+    ping $my_domain -c3 | grep '[0-9]\+ received' || {
+        echo "Not a valid domain name: $my_domain, exit ..."
+        return
+    }
 
-sudo certbot --nginx certonly # -d your.domain.xyz
-sudo certbot certificates
+    sudo ufw allow 22
+    sudo ufw allow 80
+    sudo ufw allow 443
+    sudo ufw status
 
+    sudo apt install -y nginx snapd
+    sudo snap install core
+    sudo snap refresh core
+    sudo snap install certbot --classic
+
+    sudo certbot --nginx certonly -d $my_domain
+    sudo certbot certificates
+}
+
+do_setup
